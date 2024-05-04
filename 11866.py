d9dd8e2317b0af524bc386dceab79a3319c00165
JunyoungCho07 <166793687+JunyoungCho07@users.noreply.github.com>
2024-05-04 20:43:02 +0900

    Update 11866.py

--- 11866.py
+++ 11866.py
@@ -4,7 +4,7 @@ l = [i for i in range(1,n+1)]
 result = []
 p=0
 for i in range(n):
-  p=p+k-1#k씩 건너뛰며 제거
+  p=p+k-1#k씩 건너뛰며 제거 값 제거하면 1개씩 밀리므로 -1이 있는것임
   while p >= len(l):#인덱스 밖일때 값 조정
     p = p-len(l)
   result.append(l.pop(p))#요세푸스 순열을 순서대로 제거 및 append
