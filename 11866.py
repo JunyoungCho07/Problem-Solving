aedce954384c0ef12c0424bee82e99d34aa5ebce
JunyoungCho07 <166793687+JunyoungCho07@users.noreply.github.com>
2024-05-04 19:55:06 +0900

    Create 11866.py

---
+++ 11866.py
@@ -0,0 +1,21 @@
+#11866
+n,k = map(int,input().split())
+l = [i for i in range(1,n+1)]
+result = []
+p=0
+for i in range(n):
+  p=p+k-1#k씩 건너뛰며 제거
+  while p >= len(l):#인덱스 밖일때 값 조정
+    p = p-len(l)
+  result.append(l.pop(p))#요세푸스 순열을 순서대로 제거 및 append
+
+#output
+for i in range(n):
+  if i == 0:
+    print("<",end="")
+  if i != n-1:
+    print(result[i], end=", ")
+  elif i == n-1:
+    print(result[i],end = "")
+    print(">")
+  
